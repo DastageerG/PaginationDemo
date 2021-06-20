52283af9d60935607f09eb880eca0f4dd700960f
Dastageerg <dastageerg44@gmail.com>
2021-06-19 22:31:55 -0700

    data from server

--- app/src/main/java/com/example/apitesting/MainActivity.kt
+++ app/src/main/java/com/example/apitesting/MainActivity.kt
@@ -2,12 +2,82 @@ package com.example.apitesting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.example.apitesting.data.model.ApiResponse
+import com.example.apitesting.databinding.ActivityMainBinding
+import com.example.apitesting.viewModel.MainViewModel
+import com.example.testingapp.utils.Constants
+import com.example.testingapp.utils.Constants.TAG
+import com.example.testingapp.utils.NetworkResponse
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity()
 {
+
+    private var _binding: ActivityMainBinding?=null
+    private val binding get() = _binding!!
+    private val viewModel: MainViewModel by viewModels()
+   // private val adapter by lazy { UsersAdapter() }
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.searchUser(50,1,"a")
+        viewModel.userResponse.observe({lifecycle})
+        {
+            it.let()
+            {
+                Log.d(TAG, "onCreate: "+it.data?.data?.users)
+            } // it
+        } // viewModel
+
+
+    } // onCreate closed
+
+    private fun getUserData(response: NetworkResponse<ApiResponse>?)
+    {
+        when(response)
+        {
+            is NetworkResponse.Success ->
+            {
+                hideProgressBar()
+                response.data?.data?.users.let()
+                {
+                    //     adapter.submitList(it)
+                } // response closed
+            } // is Success closed
+
+            is NetworkResponse.Error ->
+            {
+                hideProgressBar()
+                Toast.makeText(this,"No Internet Connection", Toast.LENGTH_SHORT).show()
+                Log.d(Constants.TAG, "requestApiData: "+response.message)
+            } /// is Error closed
+
+            is NetworkResponse.Loading ->
+            {
+                showProgressBar()
+            } // isLoading closing
+
+        } // when closed
+    } /// getUsersData closed
+
+
+    fun showProgressBar()
+    {
+     //   binding.progressBar.visibility = View.VISIBLE
+    } // showProgressBar
+
+    fun hideProgressBar()
+    {
+      //  binding.progressBar.visibility = View.GONE
+    } // showProgressBar
+
+
+} // class closed
